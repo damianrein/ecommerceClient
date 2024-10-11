df611ccf285a4a309c548199661738f5ba409956
damianrein <damianrein464@gmail.com>
2024-10-10 22:20:30 -0300

    -

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthServiceService } from 'src/app/services/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authServiceService: AuthServiceService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(loginForm: NgForm){
+    const email = loginForm.value.email;
+    const password = loginForm.value.password;
+
+    this.authServiceService.login(email, password).subscribe({
+      next : (response) => {
+        const token = response.token;
+
+        localStorage.setItem("jwtToken", token);
+
+        this.router.navigate(['/home']);
+      },
+      error: (error) =>{
+        console.error('Error en el login', error);
+        let errorMessage = 'Error en el inicio de sesión';
+        
+        if (error.status === 401) {
+          errorMessage = 'Credenciales incorrectas. Por favor, inténtalo de nuevo.';
+        } else if (error.status === 0) {
+          errorMessage = 'No se puede conectar al servidor. Intenta más tarde.';
+        }
+
+        alert(errorMessage);
+      }
+  });
+  }
 }
--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  apiUrl = "localhost:8080/auth";
+
+  constructor(private http: HttpClient) { }
+
+  login(email: String, password:String): Observable<any> {
+    return this.http.post<any>('${this.apiUrl}/login', {email, password});
+  }
 }
