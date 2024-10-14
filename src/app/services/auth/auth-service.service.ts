bf63eb847b91774f023db969492e6fc5fbdf5597
damianrein <damianrein464@gmail.com>
2024-10-14 19:25:26 -0300

    -

--- src/app/services/auth/auth-service.service.ts
+++ src/app/services/auth/auth-service.service.ts
@@ -9,9 +9,20 @@ export class AuthServiceService {
 
   apiUrl = "localhost:8080/auth";
 
+  LoggedIn = false;
+
   constructor(private http: HttpClient) { }
 
   login(email: String, password:String): Observable<any> {
+    this.LoggedIn = true;
     return this.http.post<any>('${this.apiUrl}/login', {email, password});
+  }
+
+  isLoggedIn(){
+    return this.LoggedIn;
+  }
+
+  logout(){
+    this.LoggedIn = false;
   }
 }
